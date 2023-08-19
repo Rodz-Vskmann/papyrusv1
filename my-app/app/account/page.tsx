ccb9711585d19600fd62098dbc98c7dcdbaa4d4b
Rodz-Vskmann <augustozuanazzi03@gmail.com>
2023-08-19 20:47:18 -0300

    adicionado a página de login

--- my-app/app/account/page.tsx
+++ my-app/app/account/page.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import './styles.css';
 
-const UserPage = () => {
+const BlankPage = () => {
   return (
-    <div className="user-page">
-      <header className="header">
-        <img className="profile-image" src="profile.jpg" alt="User Profile" />
-        <h1 className="username">Nome do Usuário</h1>
-        <p className="description">Desenvolvedor de Software | Estudante de Direito</p>
-      </header>
-      <section className="about">
-        <h2>Sobre Mim</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-          pulvinar facilisis justo mollis, auctor consequat urna.</p>
-      </section>
-      <section className="skills">
-        <h2>Habilidades</h2>
-        <div className="skill">JavaScript <div className="progress"><div className="progress-bar js"></div></div></div>
-        <div className="skill">React <div className="progress"><div className="progress-bar react"></div></div></div>
-        {/* Adicione mais habilidades aqui */}
-      </section>
-      <section className="contact">
-        <h2>Contato</h2>
-        <p>Email: exemplo@email.com</p>
-        <p>Telefone: (00) 00000-0000</p>
-      </section>
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default UserPage;
+export default BlankPage;
