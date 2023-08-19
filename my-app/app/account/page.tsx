a0932e15fb3362fd12a2c4998fe604f46be8c472
Rodz-Vskmann <augustozuanazzi03@gmail.com>
2023-08-19 20:36:30 -0300

    alteração página ''account''

--- my-app/app/account/page.tsx
+++ my-app/app/account/page.tsx
@@ -1,9 +1,32 @@
-export default function Page () {
-    return <ul className="nav_menu_list">
-    <li className="nav_menu_item">
-      <a href={''} className="nav_menu_link">
-        account
-      </a>
-    </li>
-  </ul>
-}
+import React from 'react';
+import './styles.css';
+
+const UserPage = () => {
+  return (
+    <div className="user-page">
+      <header className="header">
+        <img className="profile-image" src="profile.jpg" alt="User Profile" />
+        <h1 className="username">Nome do Usuário</h1>
+        <p className="description">Desenvolvedor de Software | Estudante de Direito</p>
+      </header>
+      <section className="about">
+        <h2>Sobre Mim</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+          pulvinar facilisis justo mollis, auctor consequat urna.</p>
+      </section>
+      <section className="skills">
+        <h2>Habilidades</h2>
+        <div className="skill">JavaScript <div className="progress"><div className="progress-bar js"></div></div></div>
+        <div className="skill">React <div className="progress"><div className="progress-bar react"></div></div></div>
+        {/* Adicione mais habilidades aqui */}
+      </section>
+      <section className="contact">
+        <h2>Contato</h2>
+        <p>Email: exemplo@email.com</p>
+        <p>Telefone: (00) 00000-0000</p>
+      </section>
+    </div>
+  );
+};
+
+export default UserPage;
